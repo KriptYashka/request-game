b2010c0fff76dbed6eb275a1f77b7d3f816eb7ff
Kuchuk Egor <kriptyashka@gmail.com>
2024-11-16 22:46:58 +0300

    Получение вопроса и отправка ответа

--- data.py
+++ data.py
@@ -1,3 +1,12 @@
+import json
+
+COLORS = {
+    "unknown": 0,
+    "red": 1,
+    "green": 2,
+    "blue": 3
+}
+
 class Team:
     def __init__(self, name: str):
         self.name = name.upper()
@@ -6,32 +15,38 @@ class Team:
 
     def solve(self, qid: int) -> bool:
         if qid in self.solved_id:
-            return False
+            return -1
         self.solved_id.add(qid)
         # TODO: добавить очки
-        return True
+        return 1
 
 
 class Field:
-    COLORS = {
-        "unknown": 0,
-        "red": 1,
-        "green": 2,
-        "blue": 3
-    }
     def __init__(self, n=20, m=20):
         self.field = [[0] * m for _ in range(n)]
 
     def set_pos(self, x, y, team: Team):
-        color = self.COLORS[team.name]
+        color = COLORS[team.name]
         self.field[y][x] = color
 
 class Questions:
     def __init__(self):
-        pass
+        with open("quests.json", encoding="UTF-8") as f:
+            self.data = json.load(f)
 
 class Game:
-    red   = Team("red")
-    green = Team("green")
-    blue  = Team("blue")
+    teams = {
+        "red"  : Team("red"),
+        "green": Team("green"),
+        "blue" : Team("blue")
+    }
+
     field = Field()
+    questions = Questions()
+
+    @classmethod
+    def solve(cls, qid: str, team_name: str):
+        team_name = team_name.lower()
+        if team_name not in cls.teams:
+            return -2
+        return cls.teams[team_name].solve(qid)
--- main.py
+++ main.py
@@ -1,20 +1,60 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, jsonify
 from data import Game
+import json
 
 app = Flask(__name__)
+header = {'Content-Type': 'text/html;charset=utf-8'}
 
 @app.route('/')
 def hello():
   return render_template('index.html')
 
-@app.route('/question/<qid>')
-def question(qid: str):
-    try:
-        qid = int(qid)
-    except TypeError as e:
-        return Response("Parameter 'id' must be integer")
+@app.route('/question/<qid>', methods=["GET", "POST"])
+def question_view(qid: str):
+    data = Game.questions.data
+    if qid not in data:
+        error = f"Вопроса с id '{qid}' не существует."
+        return Response(error, status=400)
+    question = data[qid]
+    description = question["question"]
+    answers = question["answer"]
+    score = question["score"]
+
+    text = "<h2>Неверный метод запроса.</h2>"
     if request.method == "GET":
-        data =
+        text = f"<h2>Вопрос #{qid}<h2><hr>Награда за ответ: {score}<br>{description}"
+    if request.method == "POST":
+        keys = ["team", "answer"]
+        response_body: dict[str, str] = {}
+        missing_keys = []
+        try:
+            s = request.data.decode('utf8')
+            request_json = json.loads(s)
+        except:
+            return Response("Ошибка перевода JSON в текст", status=400)
+        for key in keys:
+            if key not in request_json:
+                missing_keys.append(key)
+            else:
+                response_body[key] = request_json[key]
+        if missing_keys:
+            return Response(f"В теле запроса отсутствуют следующие ключи: {missing_keys}")
+
+        if response_body["answer"] in answers:
+            code = Game.solve(qid, response_body["team"])
+            if code == 1:
+                text = "Ответ <b>правильный</b>!"
+            elif code == -2:
+                text = f"Команды {response_body['team']} не существует."
+            elif code == -1:
+                text = "Ответ правильный!<br>Ваша команда уже ответила на этот вопрос."
+
+        elif response_body["answer"].lower() in map(lambda s: s.lower(), answers):
+            text = "Обратите внимание на регистр. Это важно."
+        else:
+            text = "Ответ <b>неверный</b>!"
+
+    return Response(text, status=200)
 
 
 def main():
